56743a87722d01abaa06081a0f3d97f8b90ac5fb
Garrett Graupmann <ggrpmnn+github@gmail.com>
2018-02-01 12:29:33 -0800

    Fixed regex and other errors.

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"regexp"
+	"strings"
+
 	"log"
 	"os"
 )
+
+var debugOn = false
 
 // AuthEntry keeps track of the login attempts on a per-IP basis
 type AuthEntry struct {
@@ -20,11 +26,11 @@ func main() {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	log.Println("auth file loaded")
+	debug("auth file loaded")
 
 	attempts := make(entries, 0)
 	// example auth log line for invalid entries: "Feb  1 19:02:48 grpi sshd[8749]: Invalid user pi from 202.120.42.141"
-	rx := regexp.MustCompile(`(\w{3}\s+\d+\s+\d{2}:\d{2}\d{2})\s+grpi sshd[\d+]: Invalid user (.*) from (\d{1,3}\.\d{1,3}\.d{1,3})`)
+	rx := regexp.MustCompile(`(\w+\s+\d+\s+\d{2}:\d{2}:\d{2})\s+grpi sshd\[\d+\]: Invalid user (.*) from (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -34,16 +40,18 @@ func main() {
 		}
 		// matches[0]=full string, [1]=date, [2]=user, [3]=IP
 		if attempts.exists(matches[3]) {
-			attempts[matches[3]].addUser(matches[2])
+			tmpEntry := attempts[matches[3]]
+			tmpEntry.addUser(matches[2])
+			attempts[matches[3]] = tmpEntry 
 		} else {
-			new := AuthEntry{count: 1, users: []string{matches[2]}}
-			attempts[matches[3]] = new
+			newEntry := AuthEntry{count: 1, users: []string{matches[2]}}
+			attempts[matches[3]] = newEntry
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	log.Println("finished parsing log file")
+	debug("finished parsing log file")
 
 	attempts.print()
 }
@@ -54,8 +62,8 @@ func (e entries) exists(ip string) bool {
 	return ok
 }
 
-func (e entries) print() {
-	for ip, ae := range entries {
+func (e *entries) print() {
+	for ip, ae := range *e {
 		fmt.Printf("IP: %s, Attempt Count: %d, Users: %s\n", ip, ae.count, strings.Join(ae.users, ", "))
 	}
 }
@@ -70,3 +78,10 @@ func (ae *AuthEntry) addUser(user string) {
 	}
 	ae.users = append(ae.users, user)
 }
+
+func debug(msg string) {
+	if debugOn {
+		log.Println(msg)
+	}
+}
+
